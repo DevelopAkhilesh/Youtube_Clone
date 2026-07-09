1c659738d3a472f9412ff9ad2755be62202449d4
akhilesh guleria <developakhilesh01@gmail.com>
2026-07-09 12:22:58 +0530

    the categories for videos

---
+++ frontend/src/constants/categories.js
@@ -0,0 +1,9 @@
+// Must stay in sync with backend enum (Video.model.js)
+export const CATEGORIES = [
+  "Web Development",
+  "JavaScript",
+  "Data Structures",
+  "Music",
+  "Gaming",
+  "Education",
+];
